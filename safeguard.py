07e596417f10291b003ed210605f6033676c8589
Randle <57602531+hostingtcp@users.noreply.github.com>
2022-04-26 11:29:53 -0800

    Update safeguard.py

--- safeguard.py
+++ safeguard.py
@@ -1,8 +1,10 @@
-# (Version) v1.0
+# (Version) v1.5
 # (Author) Randle/tcpdev
 # (Description) Entry Level SafeGuardAuth API Wrapper
+# (PyV) 3.10.4
 
 import datetime
+from tkinter import Variable
 import uuid
 from datetime import datetime, timedelta
 from typing import List, Optional
@@ -11,13 +13,11 @@ import requests
 
 
 class Safeguard:
-    def __init__(self, Username, Password, ProgramID, ProgramName) -> None:
+    def __init__(self, Username: str, Password: str) -> None:
         ''' Gather Required SafeGuard Data'''
         self.Username: str = Username # Regular SafeGuard Username
         self.Password: str = Password # This Your Encrypted Safeguard Password, Read README.md
-        self.ProgramID: hex = uuid.UUID(ProgramID).hex # SafeGuards' ProgramID is GUID
-        self.ProgramName: str = ProgramName # SafeGuard Program Name
-        
+                
         self.s = requests.session()
         
         ''' Required SafeGuard Headers'''
@@ -28,6 +28,14 @@ class Safeguard:
             'Password': self.Password
         })
         
+        ''' Get Program Information '''
+        prog_info = self.ProgramInformation(dict=True)
+        
+        self.ProgramID = prog_info['ProgramKey']
+        self.ProgramName = prog_info['Name']
+        
+        print(f"Program ID: {self.ProgramID}\nProgram Name: {self.ProgramName}")
+                
     def GenerateToken(self, days: int, level: int) -> str:
         ''' Generate SafeGuard Token '''
         
@@ -48,7 +56,7 @@ class Safeguard:
             return str(token_information['Token1'])
             #return f"Register Token: {token_information['Token1']}\nDownload Link: {self.DownloadLink()}"
         else: return f"Unable to generate token, please check you've set your login information correctly."
-            
+                
     def HWIDReset(self, username: str) -> str:
         ''' Reset Users' HWID '''
 
@@ -84,65 +92,44 @@ class Safeguard:
             - "unban" - Unban User
             - "level" - Update User's Level
         '''
-        try:
-            option = option.lower()
-            
-            program_users_resp = self.s.get("https://safeguardauth.us/api//ProgramUsers")
-            if program_users_resp.status_code == 200:
-                user_info: dict = [user for user in program_users_resp.json() if user['UserName'].lower() == username.lower()][0]
-                
-                # Required Data for SafeGuard
-                post_data: dict = {
-                    'AID': user_info['AID'],
-                    'AmountEarned': user_info['AmountEarned'],
-                    'Banned': user_info['Banned'],
-                    'Email': user_info['Email'],
-                    'ExpirationDate': user_info['ExpirationDate'],
-                    'FullName': user_info['FullName'],
-                    'GeneratedAccounts': user_info['GeneratedAccounts'],
-                    'HID': user_info['HID'],'Id': user_info['Id'],
-                    'IsMobile': user_info['IsMobile'],
-                    'Level': user_info['Level'],
-                    'Notifications': user_info['Notifications'],
-                    'Password': user_info['Password'],
-                    'ProgramId': self.ProgramID,
-                    'ProgramName': self.ProgramName, # Program Name Use Here
-                    'PurchaseDate': user_info['PurchaseDate'],
-                    'UserName': user_info['UserName']
-                }
-                
-                return_msg: str = ""
-                
-                if option == "expiration":
-                    OldExpiration = datetime.fromisoformat(user_info['ExpirationDate'].split('.')[0])
-
-                    ''' Update Expiration Date According to Expiration '''
-                    CurrentDate = datetime.now()
-                    if OldExpiration < CurrentDate: NewExpiration = CurrentDate + timedelta(int(data))
-                    else: NewExpiration = OldExpiration + timedelta(int(data))
-
-                    post_data['ExpirationDate'] = NewExpiration.isoformat()
-                    return_msg = f"*{username}:* `Expiration Date Updated - {post_data['ExpirationDate']}`"
-                    
-                elif option == "ban":
-                    post_data['Banned'] = True
-                    return_msg = f"*{username}:* `Banned`"
-                    
-                elif option == "unban":
-                    post_data['Banned'] = False
-                    return_msg = f"*{username}:* `Unbanned`"
-                    
-                elif option == "level":
-                    post_data['Level'] = data
-                    return_msg = f"*{username}:* `Level Updated - {data}`"
-            
-                ''' Update User's Information, Return Message Accordingly '''
-                update_user_req = self.s.post("https://safeguardauth.us/api//ProgramUsers", data=post_data)
-                return return_msg if update_user_req.status_code == 200 else "Unable to update user, please check you've set your login information correctly."
+        option = option.lower()
         
-        except Exception as e: print(e) 
-         
+        program_users_resp = self.s.get("https://safeguardauth.us/api//ProgramUsers")
+        if program_users_resp.status_code == 200:
+            user_info: dict = [user for user in program_users_resp.json() if user['UserName'].lower() == username.lower()][0]
             
+            user_info['ProgramId'] = self.ProgramID
+            user_info['ProgramName'] = self.ProgramName
+            
+            return_msg: str = ""
+            
+            if option == "expiration":
+                OldExpiration = datetime.fromisoformat(user_info['ExpirationDate'].split('.')[0])
+
+                ''' Update Expiration Date According to Expiration '''
+                CurrentDate = datetime.now()
+                if OldExpiration < CurrentDate: NewExpiration = CurrentDate + timedelta(int(data))
+                else: NewExpiration = OldExpiration + timedelta(int(data))
+
+                user_info['ExpirationDate'] = NewExpiration.isoformat()
+                return_msg = f"*{username}:* `Expiration Date Updated - {user_info['ExpirationDate']}`"
+                
+            elif option == "ban":
+                user_info['Banned'] = True
+                return_msg = f"*{username}:* `Banned`"
+                
+            elif option == "unban":
+                user_info['Banned'] = False
+                return_msg = f"*{username}:* `Unbanned`"
+                
+            elif option == "level":
+                user_info['Level'] = data
+                return_msg = f"*{username}:* `Level Updated - {data}`"
+        
+            ''' Update User's Information, Return Message Accordingly '''
+            update_user_req = self.s.post("https://safeguardauth.us/api//ProgramUsers", data=user_info)
+            return return_msg if update_user_req.status_code == 200 else "Unable to update user, please check you've set your login information correctly."
+                
     def TokenCreator(self, username: str) -> str:
         ''' Return Token Creator/Token Information for a User '''
         
@@ -151,7 +138,7 @@ class Safeguard:
             user_info: dict = [user for user in program_tokens_resp.json() if str(user['ConsumedBy']).lower() == username.lower()][0]
             if user_info: return f"*Token:* `{user_info['Token1']}`\n*Created By:* `{user_info['CreatedBy']}`\n*Days:* `{user_info['Days']}`\n*Level:* `{user_info['UserLevel']}`"
             else: return "Unable to find user"
-        else: return "Unable to find user, please check you've set your login information correctly."
+        else: raise Exception("Unable to find program, please check you've set your login information correctly.")
     
     def FailLogs(self, username: str) -> str:
         ''' Return Past 5 Entire in Fail Logs '''
@@ -169,4 +156,49 @@ class Safeguard:
                     else: break
             return_msg = '\n'.join(return_list)
             return f"Username: {username}\nFail Reasons:\n`{return_msg}`"
-        else: return "Unable to find user, please check you've set your login information correctly."
+        else: raise Exception("Unable to find program, please check you've set your login information correctly.")
+
+    def ProgramInformation(self, dict: bool = False) -> dict or str:
+        ''' Return Program Information '''
+        try:
+            program_info_resp = self.s.get("https://safeguardauth.us/api//AllPrograms")
+            if program_info_resp.status_code == 200:
+                program_info: dict = program_info_resp.json()[0]
+                if dict: return program_info
+                else: return f"Program Name: {program_info['Name']}\nProgram ID: {program_info['ProgramKey']}\nProgram Version: {program_info['AutoUpdateVersion']}\nProgram Download: {program_info['AutoUpdateUrl']}\nProgram Expiration: {program_info['ExpirationDate']}"
+            else: raise Exception("Unable to find program, please check you've set your login information correctly.")
+        except Exception as e: print(e)  
+        
+    def UpdateProgram(self, option: str, data: Optional[vars] = None) -> str:
+        ''' Update Program Information '''
+        
+        ''' Availabe arguments for 'option':
+            - "Enable" - True/False
+            - "CheckHwid" - True/False
+            - "AutoUpdate" - True/False
+            - "AutoUpdateVersion" - Version Number
+            - "AutoUpdateUrl" - Download Link
+        '''
+        
+        option = option.lower()
+    
+        program_data_resp = self.s.get("https://safeguardauth.us/api//AllPrograms")
+        if program_data_resp.status_code == 200:
+            program_info: dict = program_data_resp.json()[0]
+            
+            return_msg: str = f"{option}: {data}"
+            
+            match_input: dict = {
+                'enable': 'IsEnabled',
+                'checkhwid': 'CheckHwid',
+                'autoupdate': 'AutoUpdate',
+                'autoupdateversion': 'AutoUpdateVersion',
+                'autoupdateurl': 'AutoUpdateUrl'
+            }
+            
+            if option in match_input: 
+                program_info[match_input[option]] = data
+                
+            update_program_req = self.s.post("https://safeguardauth.us/api//UpdateProgram", data=program_info)
+            return return_msg if update_program_req.status_code == 200 else "Unable to update program, please check you've set your login information correctly."  
+        else: raise Exception("Unable to find program, please check you've set your login information correctly.")
